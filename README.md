e328ad192dc02e06fadd220d91f3dbad679e4f7f
Pavel Paranin <pavel.paranin@firma.seznam.cz>
2021-01-08 17:11:33 +0100

    🚀 Expanding the project info

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
-# How to launch the project
-1) Install the project via `pip`: `pip install .`
-> Next part is WIP
+# 🚀 How to launch the project
+1) You can choose any method to install the project into your virtual (or global) python environment
+   
+   - via `pip` from the project's root
+      > pip install .
+
+      #### or
+   
+   - via pip from `github`
+      > pip install -U git+https://github.com/vrppaul/spaceship.git
+
+#### Next part is WIP
 2) 
     - Launch the project with `spaceship launch` (try `spaceship launch --help` for additional setup)
     - Get the list of commands with `spaceship`
+
+
+## List of emojies to prettify commits
+
+> 🚀, 🔧
--- setup.py
+++ setup.py
@@ -1,14 +1,28 @@
 from setuptools import find_packages, setup
 
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
 setup(
+    author="Pavel Paranin",
+    author_email="pavel@paran.in",
     name="spaceship",
-    version="0.0.0",
+    version="0.0.1",
     description="Cosmic web-ecosystem",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     setup_requires=["wheel", "setuptools"],
     packages=find_packages(),
     entry_points="""
     [console_scripts]
     spaceship=spaceship.control_center.dispatcher:control
-    """
+    """,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
 )
