e0a1af76643057aa0adf4990a48aa290920f4e79
Pavel Paranin <pavel.paranin@firma.seznam.cz>
2021-01-08 16:12:15 +0100

    🔧 correct packages and dependencies

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from distutils.core import setup
+from setuptools import find_packages, setup
 
 
 setup(
     name="spaceship",
     version="0.0.0",
     description="Cosmic web-ecosystem",
+    install_requires=["wheel", "setuptools"],
+    packages=find_packages(),
     entry_points="""
     [console_scripts]
     spaceship=spaceship.control_center.dispatcher:control
